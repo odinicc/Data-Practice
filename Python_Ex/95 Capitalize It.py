31c255f31cd4ce191142a6284eb14252a1ffdd42
odinicc <100484183+odinicc@users.noreply.github.com>
2022-04-26 21:00:51 +0300

    Update 95 Capitalize It.py

--- Python_Ex/95 Capitalize It.py
+++ Python_Ex/95 Capitalize It.py
@@ -1,13 +1,33 @@
 
+a = " enter the i yes? word "
+
+def capitalize(a):
+
+    ch = list(a)
+
+    # Capitalize the first non-space character
+    for i in range(len(ch)-1):
+        if ch[i] != " ":
+            ch[i] = ch[i].upper()
+            break
+
+    # Capitalize the first non-space character after a period  exclamation mark or question mark
+    for i in range(len(ch)-1):
+        if ch[i] == "?" or ch[i] == "!":
+            for k in range(i+1 , len(ch)-1):
+                if ch[k] != " ":
+                    ch[k] = ch[k].upper()
+                    break
+
+    #Upper case i
+    for i in range(len(ch)-3):
+        if ch[i] == " " and ch[i+1] == "i" and ch[i+2] == " ":
+            ch[i+1] = ch[i+1].upper()
+    str1 = ''.join(ch)
+    return str1 
+
+print(a)
+print(capitalize(a))
 
 
-def Convert(string):
-    li = list(string.split(" "))
-    return li
 
-St = "I love simmer"
-
-print(type(St))
-lis = Convert[St]
-
-#print(lis[1])
