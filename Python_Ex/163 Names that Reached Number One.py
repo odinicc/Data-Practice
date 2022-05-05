bca667462c5005133b1d49772cfab5898bd2abc6
odinicc <100484183+odinicc@users.noreply.github.com>
2022-05-05 09:47:06 +0300

    Go

--- Python_Ex/163 Names that Reached Number One.py
+++ Python_Ex/163 Names that Reached Number One.py
@@ -1,6 +1,9 @@
 from tqdm import tqdm
 from itertools import islice
 import pprint
+from pathlib import Path
+import collections
+
 file_path = "C:\\Users\\ivan\\Documents\\GitHub\\Analyzing-Data-with-Python\\Python_Ex\\baby_names\\"
 file_name = "yob1880.txt"
 F_names ={}
@@ -62,5 +65,33 @@ m_items = take(30, M_names_sorted.items())
 f_items = take(30, F_names_sorted.items())
 
 pp = pprint.PrettyPrinter(sort_dicts=False)
-pp.pprint(m_items)
-pp.pprint(f_items)
+#pp.pprint(m_items)
+#pp.pprint(f_items)
+#pp.pprint(Hender)
+
+for i in range(10) :
+    year = input("Give me the year ( 0 - for exit) ")
+    if int(year) == 0:
+        break
+    ff_path = file_path + "yob" + str(year) + ".txt"
+    path = Path(ff_path)
+    if path.is_file() == False:
+        print("File for year", year, "don't exist")
+    else:
+        inf = open(path, 'r')
+        Lines = inf.readlines()
+        k = len(Lines)
+        year_trans_names = collections.defaultdict(dict)
+        year_trans_names_2 = collections.defaultdict(dict)
+        for row in range(k):
+            words = Lines[row].split(",")
+            words[2] = words[2].replace('\n', '')
+            if Hender[words[0]] == 'T':
+                year_trans_names[words[0]][words[1]] = words[2]
+
+        for row in year_trans_names:
+            if ("M" in year_trans_names[row]) and ("F" in year_trans_names[row]):
+                year_trans_names_2[row] = year_trans_names[row]
+        pp.pprint(year_trans_names_2)
+
+
