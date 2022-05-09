5b30c0159beca55b9554688ce62cdc7613558870
odinicc <100484183+odinicc@users.noreply.github.com>
2022-05-09 21:27:04 +0300

    pass

---
+++ Stepik - Алгоритмы Теория и Практика/4 Different sum.py
@@ -0,0 +1,24 @@
+
+
+def difsum(num):
+    s = 0
+    app = []
+    if num ==1:
+        return [1] , 1
+    for i in range(1, num):
+        if s < num < s + i :
+            last = num - sum(app)
+            app[i-2] += last
+            return app, i-1
+        elif s+i == num or num == 2:
+            last = num - sum(app)
+            app.append(last)
+            return app, i
+        s = s + i
+        app.append(i)
+
+
+num = int(input())
+app , i= difsum(num)
+print(i)
+print(*app)
--- Stepik - Алгоритмы Теория и Практика/5 continuous backpack.py
+++ Stepik - Алгоритмы Теория и Практика/5 continuous backpack.py
@@ -1,49 +1,45 @@
 import random
-import pprint
 import sys
 
-#input = open('1.txt', 'r') //расскомментировать решая задачу локально
-# input = sys.stdin //расскомментировать при сдаче задачи в системе
+#for local solution
+inf = open('1.txt', 'r')
 
+#for system solution
+# inf = sys.stdin
 
-S = []
-n = int(input.readline())
-for i in range(1,n+1):
-  x,y = map(int, input.readline().split())
-  S.append([x,y])
+Lines = inf.readlines()
 
+First_Line = Lines[0]
+words = First_Line.split(" ")
+back_pack = int(words[1].replace('\n', ''))
+n = int(words[0])
+Lines.pop(0)
 
-back_pack = 20
-n = 3
 goods = []
 
-for i in range(10):
-    c = []
-    a = random.randint(1, 100)
-    b = random.randint(1, 100)
-    c.append(a)
-    c.append(b)
-    goods.append(c)
-
-n , back_pack = map(int, input().split())
-
-
+for Line in Lines:
+    words = Line.split(" ")
+    price = int(words[0])
+    weight = int(words[1].replace('\n', ''))
+    goods.append([price,weight])
 
 
 for i in range(len(goods)):
 
-    d = round((goods[i][0])/(goods[i][1]),3)
+    d = round((goods[i][0])/(goods[i][1]),10)
     goods[i].append(d)
 
 goods.sort(reverse=True,key=lambda elem: elem[2])
 
 free_back_pack = back_pack
 overal_back_pack_cost = 0
+
 for i in range(len(goods)):
     if goods[i][1]< free_back_pack:
         overal_back_pack_cost += goods[i][0]
         free_back_pack -= goods[i][1]
     else:
-        overal_back_pack_cost += round(goods[i][2]*free_back_pack,3)
+        overal_back_pack_cost += round(goods[i][2]*free_back_pack,10)
+        free_back_pack = 0
 
-print(overal_back_pack_cost)
+print(round(overal_back_pack_cost,3))
