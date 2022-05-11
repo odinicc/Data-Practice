44eea575f83c17f0e55c024d6b00a412deab509a
odinicc <100484183+odinicc@users.noreply.github.com>
2022-05-11 21:40:04 +0300

    hi

--- Stepik - Алгоритмы Теория и Практика/11 Merge  Sort.py
+++ Stepik - Алгоритмы Теория и Практика/11 Merge  Sort.py
@@ -1,5 +1,6 @@
 import math
 import sys
+inv = 0
 
 def read_data():
 
@@ -12,26 +13,11 @@ def read_data():
     lines = inf.readlines()
 
     secondline_line_input = lines[1].replace('\n', '')
-    arr_2 = secondline_line_input.split(" ")
-    arr_2.pop(0)
-    arr_2 = list(map(int, arr_2))
+    arr = secondline_line_input.split(" ")
+    arr = list(map(int, arr))
 
-    return arr_2
-''' 
-def merge_sort(arr):
-    if len(arr) == 1:
-        return arr
-    elif len(arr) == 2:
-        if arr[0]<arr[1]:
-            return arr
-        else:
-            return [arr[1],arr[0]]
-    else:
-        median = math.floor(len(arr) / 2)
-        arr_1 = merge_sort(arr[:median])
-        arr_2 = merge_sort(arr[median:])
-        return merge_func(arr_1 , arr_2)
-'''
+    return arr
+
 def merge_sort(arr):
     if len(arr) == 1:
         return arr
@@ -40,9 +26,10 @@ def merge_sort(arr):
         arr_1 = merge_sort(arr[:median])
         arr_2 = merge_sort(arr[median:])
         return merge_func(arr_1 , arr_2)
-
 
 def merge_func(arr_1 , arr_2):
+    global inv
+
     it_arr_1 = 0
     it_arr_2 = 0
     result = []
@@ -50,37 +37,35 @@ def merge_func(arr_1 , arr_2):
         if len(arr_1) - it_arr_1 > 0 and len(arr_2) - it_arr_2 > 0:
             if arr_1[it_arr_1] <= arr_2[it_arr_2]:
                 result.append(arr_1[it_arr_1])
+                print("arr_1 " , arr_1 , "it_arr_1 " , it_arr_1,"arr_2 " , arr_2 , "it_arr_2 " , it_arr_2 )
                 it_arr_1 += 1
+
             elif arr_1[it_arr_1] > arr_2[it_arr_2]:
                 result.append(arr_2[it_arr_2])
+
                 it_arr_2 += 1
+
 
         elif len(arr_1) - it_arr_1 > 0 and len(arr_2) - it_arr_2 == 0:
             result.append(arr_1[it_arr_1])
+            #inv += len(arr_1) - it_arr_1
             it_arr_1 += 1
 
 
         elif len(arr_1) - it_arr_1 == 0 and len(arr_2) - it_arr_2 > 0:
             result.append(arr_2[it_arr_2])
+            #inv += len(arr_2) - it_arr_2
             it_arr_2 += 1
 
     return result
 
 
-
 arr = read_data()
-arr = [2,6,5,7,2,12,15,20,1,88,8]
-
-
-arr_1 = [2,6,7,12,15,20]
-arr_2 = [3,4,8,9,10,12]
-print("arr1",arr_1)
-print("arr2",arr_2)
-result  = merge_func(arr_1 , arr_2)
-print("result",result)
 
 print("arr =",arr)
+
 sort = merge_sort(arr)
+print("inv =",inv)
 print("sort = ",sort)
 
 
