e66c4aa8302f4cc0baae43e62e4e61fc2f19ae5d
odinicc <100484183+odinicc@users.noreply.github.com>
2022-05-17 17:35:18 +0300

    hi

--- Stepik - Алгоритмы Теория и Практика/02 Fibonacci fast last module.py
+++ Stepik - Алгоритмы Теория и Практика/02 Fibonacci fast last module.py
@@ -18,16 +18,22 @@ def read_data():
 def calculate_fib(N,mod):
     Fib_arr = []
     Mod_arr = []
+    Mod_arr2 = []
     if N > 0:
         Fib_arr.append(1)
+        Mod_arr2.append(1)
     if N > 1:
         Fib_arr.append(1)
+        Mod_arr2.append(1)
+        prev = 1
     if N > 2:
         for i in range(2, N):
             K = Fib_arr[i - 1] + Fib_arr[i - 2]
             Fib_arr.append(K)
             M = (Fib_arr[i - 1] + Fib_arr[i - 2])%mod
+            Mod_arr2.append(M)
             Mod_arr.append([i,K,M])
+             
 
     pprint.pprint(Mod_arr)
     return Fib_arr[-1]
