d39c9a7c2ca778a6e338d3ca28f9770d5690c680
odinicc <100484183+odinicc@users.noreply.github.com>
2022-06-18 22:39:41 +0300

    main

--- 18 Python Basics for Data Science/tester.py
+++ 18 Python Basics for Data Science/tester.py
@@ -1,2 +1,16 @@
-print("Hello world bro")
-print("How are you")
+from re import L
+import sys
+
+def recur(n):
+    for i in range(n):
+        ar = list(str(n))
+        if len(ar) == 1:
+            return ar[0]
+            break
+        n = 0
+        for i in ar:
+            n = n + int(i)
+
+
+m = recur(651)
+print("m = ", m)
