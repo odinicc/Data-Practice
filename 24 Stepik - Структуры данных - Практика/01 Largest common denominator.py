37b7bf1b5f3f1d2fbf77a77654c10b4e1da46af8
odinicc <100484183+odinicc@users.noreply.github.com>
2022-12-17 13:52:26 +0300

    main

---
+++ 24 Stepik - Структуры данных - Практика/01 Largest common denominator.py
@@ -0,0 +1,29 @@
+import sys
+
+def read_data():
+    # for local solution
+    inf = open('2fib.txt', 'r')
+
+    # for system solution
+    # inf = sys.stdin
+
+    Lines = inf.readline().replace('\n', '')
+    A = list(Lines.split(" "))
+    N = int(A[0])
+    return N
+
+def calculate_fib(N):
+    Fib_arr = []
+    if N > 0:
+        Fib_arr.append(1)
+    if N > 1:
+        Fib_arr.append(1)
+    if N > 2:
+        for i in range(2, N):
+            K = (Fib_arr[i - 1] + Fib_arr[i - 2])%10
+            Fib_arr.append(K)
+    return Fib_arr[-1]
+
+N = 6
+Fib = calculate_fib(N)
+print(Fib)
