2e183972d39023f5d22849bee5d9ee81665719ab
odinicc <100484183+odinicc@users.noreply.github.com>
2022-12-19 00:07:03 +0300

    main

--- 24 Stepik - Структуры данных - Практика/03 Max Stack 2.py
+++ 24 Stepik - Структуры данных - Практика/03 Max Stack 2.py
@@ -17,27 +17,41 @@ def read_data():
 
     return l
 
-def cust_append(stack,element):
-    x = stack.append(int(element))
-    return x
+def cust_append(stack,track_stack,element):
+    stack.append(int(element))
+    if len(track_stack) == 0:
+        track_stack.append(int(element))
+    else:
+        if int(element) > track_stack[-1]:
+            track_stack.append(int(element))
+        else:
+            track_stack.append(track_stack[-1])
+    return stack, track_stack
 
+def cust_pop(stack,track_stack):
+    stack.pop()
+    track_stack.pop()
+    return stack, track_stack
+
+def cust_max(stack,track_stack):
+    if len(stack)>0:
+        return(track_stack[-1])
+    else:
+        return 0
 
 def max_result(s):
     stack = []
+    track_stack = []
     max_stack = []
-    max_value = 0
     for i in range(len(s)):
         if s[i][0] == 'push':
-            stack = cust_append(stack,s[i][1])
-
+            stack, track_stack  = cust_append(stack,track_stack,s[i][1])
         elif s[i][0] == 'pop':
             if len(stack)>0:
-                stack.pop()
+                stack, track_stack = cust_pop(stack,track_stack)
         elif s[i][0] == 'max':
-            if len(stack)>0:
-                max_stack.append(max(stack))
-            else:
-                max_stack.append(0)
+            max_stack.append(cust_max(stack,track_stack))
+    print(track_stack)
     return  max_stack
 
 
