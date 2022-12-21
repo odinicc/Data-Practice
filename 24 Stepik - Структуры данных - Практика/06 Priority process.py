1f61dbbf48ad67d2add7ca5f5518fbcd869c420f
odinicc <100484183+odinicc@users.noreply.github.com>
2022-12-21 20:29:51 +0300

    main

--- 24 Stepik - Структуры данных - Практика/06 Priority process.py
+++ 24 Stepik - Структуры данных - Практика/06 Priority process.py
@@ -14,20 +14,40 @@ def read_data():
 
     lines = inf.readlines()
 
-    lis = lines[1].rstrip('\n')
+    f = lines[0].rstrip('\n')
+    num_proc = int(f[0])
+    s = lines[1].rstrip('\n')
+
+    lis = s.split(' ')
 
     inf.close()
-    lis = lis.split()
-    lis = list(map(int, lis))
-
-    return lis
+    return num_proc , lis
 
 
+def find_min_proc(proc):
+    min_proc = 0
+    for i in range(len(proc)-1):
+        if proc[i] < proc[min_proc]:
+            min_proc = i
+
+    return min_proc
 
 
+print(find_min_proc([3,3,4]))
+
+num_proc, lis = read_data()
 
 
+proc = []
+for i in range(num_proc):
+    proc.append(0)
+
+work = []
+
+#for i in range(len(lis)):
+#    for pr in proc:
 
 
-
+#print(num_proc)
+#print(lis)
 
