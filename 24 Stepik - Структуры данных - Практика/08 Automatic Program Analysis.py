af0e6afe930f1e6f4bc43c2a0edbcd0e5c4cdfab
odinicc <100484183+odinicc@users.noreply.github.com>
2022-12-21 10:26:01 +0300

    main

--- 24 Stepik - Структуры данных - Практика/08 Automatic Program Analysis.py
+++ 24 Stepik - Структуры данных - Практика/08 Automatic Program Analysis.py
@@ -17,32 +17,37 @@ def read_data():
     lis = lines[0].rstrip('\n')
     first_row = lis.split()
     equal_num = int(first_row[1])
-    print(first_row[1])
-    print(type(lines))
 
     equal = []
     for line in lines[1:equal_num+1]:
-        print(line)
+        el = line.rstrip('\n')
+        equal.append([el[0],el[2]])
 
     non_equal = []
     print('--------------------------')
     for line in lines[equal_num+1:]:
-        print(line)
-
+        el = line.rstrip('\n')
+        non_equal.append([el[0],el[2]])
 
     inf.close()
+    return equal , non_equal
 
 
-    return lis
+equal , non_equal  = read_data()
+print(equal)
+print(non_equal)
 
+main_set = set()
 
+for eq in equal:
+    main_set.add(eq[0])
+    main_set.add(eq[1])
 
+print(main_set)
 
-lis = read_data()
-#print(binarytree.build(lis))
-
-
-
+for neq in non_equal:
+    if (neq[0] in main_set) and (neq[1] in main_set):
+        return
 
 
 
