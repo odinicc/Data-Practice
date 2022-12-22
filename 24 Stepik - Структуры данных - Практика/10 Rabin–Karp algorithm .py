c3967d95b909f86c46438024cf5a345e9a26c166
odinicc <100484183+odinicc@users.noreply.github.com>
2022-12-22 12:18:35 +0300

    mian

--- 24 Stepik - Структуры данных - Практика/10 Rabin–Karp algorithm .py
+++ 24 Stepik - Структуры данных - Практика/10 Rabin–Karp algorithm .py
@@ -3,41 +3,50 @@
 import sys
 import math
 
-swapper = []
+
 
 def read_data():
     #read from file
-    inf = open("09 Phone book.txt","r")
+    inf = open("10 Rabin–Karp algorithm.txt","r")
 
     #read from system input
     #inf = sys.stdin
 
     lines = inf.readlines()
 
-    lis = []
-    for line in lines[1:]:
-        el = line.rstrip('\n')
-        el2 = el.split(' ')
-        lis.append(el2)
+    substr = lines[0].rstrip()
+    stri =lines[1].rstrip()
 
-    return lis
+    return list(substr) , list(stri)
 
 
+def hash_f(stri,n):
+    hash = 0
+    for i in range(len(stri)):
+        hash = hash*256 + ord(stri[i])
+    return hash%n
 
-lis  = read_data()
-phone_book = {}
-for lis_item in lis:
-    if lis_item[0] == 'add':
-        phone_book[lis_item[1]] = lis_item[2]
-    elif lis_item[0] == 'find':
-        if lis_item[1] in phone_book:
-            print(phone_book[lis_item[1]])
-        else:
-            print('not found')
+# Use Fermat algoithm
+def is_prime(num):
+    if num == 2:
+        return True
+    if not num & 1:
+        return False
+    return pow(2, num-1, num) == 1
 
-    elif lis_item[0] == 'del':
-        if lis_item[1] in phone_book:
-            del phone_book[lis_item[1]]
+def find_max_prime(n):
+    for i in range(n,0,-1):
+        if is_prime(i) == True:
+            return i
 
 
 
+substr , stri = read_data()
+
+target_prime = find_max_prime(len(substr))
+print(target_prime)
+
+print( hash_f(substr,15) )
+
+
+print(is_prime(10))
