d0e58b21c658d5a64fa354dd2d4fbe58f759f102
odinicc <100484183+odinicc@users.noreply.github.com>
2022-12-22 10:02:24 +0300

    main

--- 24 Stepik - Структуры данных - Практика/09 Phone book.py
+++ 24 Stepik - Структуры данных - Практика/09 Phone book.py
@@ -7,57 +7,37 @@ swapper = []
 
 def read_data():
     #read from file
-    inf = open("08 Automatic Program Analysis.txt","r")
+    inf = open("09 Phone book.txt","r")
 
     #read from system input
     #inf = sys.stdin
 
     lines = inf.readlines()
 
-    lis = lines[0].rstrip('\n')
-    first_row = lis.split(' ')
-    equal_num = int(first_row[1])
-    #print(equal_num,' - equal_num')
-    #print('first_row',first_row)
-
-    equal = []
-    for line in lines[1:equal_num+1]:
+    lis = []
+    for line in lines[1:]:
         el = line.rstrip('\n')
-        equal.append([el[0],el[2]])
+        el2 = el.split(' ')
+        lis.append(el2)
 
-    non_equal = []
-    for line in lines[equal_num+1:]:
-        el = line.rstrip('\n')
-        non_equal.append([el[0],el[2]])
-
-    inf.close()
-    return equal , non_equal
-
-def algo_checker(equal,non_equal):
-    main_set = set()
-
-    for eq in equal:
-        main_set.add(eq[0])
-        main_set.add(eq[1])
-
-    for neq in non_equal:
-        if (neq[0] in main_set) and (neq[1] in main_set):
-            return 0
-        #this will check situations when variable compared with itself
-        #for example 5 != 5
-        if (neq[0] == neq[1]):
-            return 0
-
-    return 1
+    return lis
 
 
 
+lis  = read_data()
+phone_book = {}
+for lis_item in lis:
+    if lis_item[0] == 'add':
+        phone_book[lis_item[1]] = lis_item[2]
+    elif lis_item[0] == 'find':
+        if lis_item[1] in phone_book:
+            print(phone_book[lis_item[1]])
+        else:
+            print('not found')
 
-equal , non_equal  = read_data()
-#print(equal)
-#print(non_equal)
-
-print(algo_checker(equal,non_equal))
+    elif lis_item[0] == 'del':
+        if lis_item[1] in phone_book:
+            del phone_book[lis_item[1]]
 
 
 
