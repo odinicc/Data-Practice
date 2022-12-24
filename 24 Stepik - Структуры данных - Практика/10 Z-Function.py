703b987fc12fc4609e2f0d6b289675627ab84b15
odinicc <100484183+odinicc@users.noreply.github.com>
2022-12-24 17:06:38 +0300

    main

--- 24 Stepik - Структуры данных - Практика/10 Z-Function.py
+++ 24 Stepik - Структуры данных - Практика/10 Z-Function.py
@@ -3,8 +3,6 @@ import hashlib
 import sys
 import math
 
-
-
 def read_data():
     #read from file
     inf = open("10 Rabin–Karp algorithm.txt","r")
@@ -20,23 +18,11 @@ def read_data():
     return list(substr) , list(stri)
 
 
-def hash_f(stri):
-    result = hashlib.md5(str(stri).encode())
-    return result
-
 
 
 
 substr , stri = read_data()
 
 substr_len = len(substr)
-hash_substr = hash_f(substr)
 
 
-for i in range(len(stri) -substr_len+1):
-    hash_cur_string = hash_f(stri[i:i+substr_len])
-    if hash_cur_string.hexdigest() == hash_substr.hexdigest():
-        if stri[i:i+substr_len] == substr:
-            print(i, end =" ")
-
-print('  ')
