b4c97debd0bdab74a5f69e2479fb3beaf0342eb0
odinicc <100484183+odinicc@users.noreply.github.com>
2022-12-24 17:38:28 +0300

    main

--- 24 Stepik - Структуры данных - Практика/10 Z-Function.py
+++ 24 Stepik - Структуры данных - Практика/10 Z-Function.py
@@ -17,12 +17,67 @@ def read_data():
 
     return list(substr) , list(stri)
 
+def z_function(stri):
+    l = len(stri)
+    z = [0] * l
+    n = len(stri)
 
+    # [L,R] make a window which matches
+    # with prefix of s
+    l, r, k = 0, 0, 0
+    for i in range(1, n):
 
+        # if i>R nothing matches so we will calculate.
+        # Z[i] using naive way.
+        if i > r:
+            l, r = i, i
+
+            # R-L = 0 in starting, so it will start
+            # checking from 0'th index. For example,
+            # for "ababab" and i = 1, the value of R
+            # remains 0 and Z[i] becomes 0. For string
+            # "aaaaaa" and i = 1, Z[i] and R become 5
+            while r < n and stri[r - l] == stri[r]:
+                r += 1
+            z[i] = r - l
+            r -= 1
+        else:
+
+            # k = i-L so k corresponds to number which
+            # matches in [L,R] interval.
+            k = i - l
+
+            # if Z[k] is less than remaining interval
+            # then Z[i] will be equal to Z[k].
+            # For example, str = "ababab", i = 3, R = 5
+            # and L = 2
+            if z[k] < r - i + 1:
+                z[i] = z[k]
+
+            # For example str = "aaaaaa" and i = 2,
+            # R is 5, L is 0
+            else:
+
+                # else start from R and check manually
+                l = i
+                while r < n and stri[r - l] == stri[r]:
+                    r += 1
+                z[i] = r - l
+                r -= 1
+
+    return z
+
+def z_search(pattern,text):
+    concat = pattern + ["$"] + text
+    l = len(concat)
+    z = z_function(concat)
+    for i in range(l):
+        if z[i] == len(pattern):
+            print(i - len(pattern) -1,end = ' ')
 
 
 substr , stri = read_data()
 
-substr_len = len(substr)
 
+z_search(substr, stri)
 
