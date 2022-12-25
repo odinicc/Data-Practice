4417b9c7572f59a2e024a99bd12d85ad13f6382d
odinicc <100484183+odinicc@users.noreply.github.com>
2022-12-25 18:50:35 +0300

    main

--- 25 Leetcode/66. Plus One.py
+++ 25 Leetcode/66. Plus One.py
@@ -5,22 +5,35 @@ import math
 def plusOne(digits):
     digits = [int(x) for x in digits]
     digits2 = []
-    temp = 1
+    temp = 0
     for i in range(len(digits)-1,-1,-1):
-        print('i = ',i)
-        if digits[i]+ temp < 9:
-            digits2.append(digits[i]+temp)
+
+        if digits[i]< 9 and temp == 0:
+            if i == len(digits)-1:
+                digits2.append(digits[i]+1)
+            else:
+                digits2.append(digits[i])
             temp = 0
-        else:
+        elif digits[i]< 9 and temp == 1:
+            digits2.append(digits[i]+1)
+            temp = 0
+        elif digits[i] == 9 and temp == 1:
             digits2.append(0)
             temp = 1
+        elif digits[i] == 9 and temp == 0:
+            if i == len(digits)-1:
+                digits2.append(0)
+                temp = 1
+            else:
+                digits2.append(digits[i])
 
     if temp == 1:
         digits2.append(1)
+
 
     return digits2[::-1]
 
 
 
-print(plusOne(list('9')))
+print(plusOne(list('9876543210')))
 
