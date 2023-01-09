9f56f18d6b77a178b0d248d04e9f245b749a47dc
odinicc <100484183+odinicc@users.noreply.github.com>
2023-01-09 19:29:48 +0300

    Main

--- 25 Leetcode/1979. Find Greatest Common Divisor of Array.py
+++ 25 Leetcode/1979. Find Greatest Common Divisor of Array.py
@@ -2,11 +2,16 @@ import math
 
 def findGCD(nums):
     n_min = min(nums)
-    print(n_min)
+    n_max = max(nums)
+    print(n_min,'--',n_max)
+    n = min(n_min,n_max)
+    for i in range(n,0,-1):
+        if n_min%i == 0 and n_max%i == 0:
+            return i
 
 
 
-nums = [2,5,6,9,10]
+nums = [3,9]
 
 
 print(findGCD(nums))
