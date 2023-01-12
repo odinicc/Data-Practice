a666ba85d9755bed0f118942eedf10ffda744166
odinicc <100484183+odinicc@users.noreply.github.com>
2023-01-12 22:37:04 +0300

    main

--- 25 Leetcode/242. Valid Anagram.py
+++ 25 Leetcode/242. Valid Anagram.py
@@ -1,13 +1,23 @@
 import math
-
+import string
 
 def isAnagram(s, t):
-    print(s,t)
-    s = list(s)
-    t = list(s)
-    print(s)
-    for j in s:
-        for i in
+    if len(s) != len(t):
+        return False
+    else:
+        alphabet = list(string.ascii_lowercase)
+        s_dic = {}
+        t_dic = {}
+        for m in alphabet:
+            s_dic[m] = 0
+            t_dic[m] = 0
+        for i in range(len(t)):
+            s_dic[s[i]] += 1
+            t_dic[t[i]] += 1
+        for m in alphabet:
+            if s_dic[m] != t_dic[m]:
+                return False
+        return True
 
 
 s = "anagram"
---
+++ 25 Leetcode/290. Word Pattern.py
@@ -0,0 +1,11 @@
+import math
+import string
+
+def wordPattern(pattern, s):
+    m_dic = {}
+
+pattern = "abba"
+s = "dog cat cat dog"
+print(wordPattern(pattern ,s))
+
+
