a395dc5c57668b725f0bd9ebd590257ef03d8add
odinicc <100484183+odinicc@users.noreply.github.com>
2023-01-12 22:16:34 +0300

    main

---
+++ 25 Leetcode/242. Valid Anagram.py
@@ -0,0 +1,17 @@
+import math
+
+
+def isAnagram(s, t):
+    print(s,t)
+    s = list(s)
+    t = list(s)
+    print(s)
+    for j in s:
+        for i in
+
+
+s = "anagram"
+t = "nagaram"
+print(isAnagram(s,t))
+
+
