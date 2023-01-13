729334d02b0ae73a4e7d8b75e46e045dfbf9ccee
odinicc <100484183+odinicc@users.noreply.github.com>
2023-01-13 21:29:49 +0300

    main

--- 25 Leetcode/387. First Unique Character in a String.py
+++ 25 Leetcode/387. First Unique Character in a String.py
@@ -4,13 +4,28 @@ import string
 #def isIsomorphic(self, s, t):
 def firstUniqChar(s):
     s = list(s)
+    md = {}
+    m_value = len(s)+1
     alphabet = list(string.ascii_lowercase)
     for a in alphabet:
-        
+        md[a] = []
+    for r in range(len(s)):
+        x = md[s[r]]
+        x.append(r)
+        md[s[r]] = x
+    for letter in md:
+        if len(md[letter]) == 1:
+            if md[letter][0] < m_value:
+                m_value = md[letter][0]
+
+    if m_value > len(s):
+        return -1
+    else:
+        return m_value
 
 
 
-s = "aadadaad"
+s = "loveleetcode"
 print(firstUniqChar(s))
 
 
