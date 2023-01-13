706054f181513dd37f2c5e68c52a8efadb838dbf
odinicc <100484183+odinicc@users.noreply.github.com>
2023-01-13 08:17:29 +0300

    Main

--- 25 Leetcode/290. Word Pattern.py
+++ 25 Leetcode/290. Word Pattern.py
@@ -1,11 +1,25 @@
 import math
 import string
 
+
+def check_value_exist(test_dict, value):
+    do_exist = False
+    for key, val in test_dict.items():
+        if val == value:
+            do_exist = True
+    return do_exist
+
 def wordPattern(pattern, s):
-    m_dic = {}
+    s = s.split(' ')
+    if len(s) != len(pattern):
+        return False
+    else:
+        print(map(pattern.find, s))
+
+
 
 pattern = "abba"
-s = "dog cat cat dog"
+s = "dog cat cat fish"
 print(wordPattern(pattern ,s))
 
 
