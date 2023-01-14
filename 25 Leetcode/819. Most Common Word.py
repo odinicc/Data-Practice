c57b7b19dd525555050f47b32dd0fc1f95307bc4
odinicc <100484183+odinicc@users.noreply.github.com>
2023-01-14 11:22:32 +0300

    main

--- 25 Leetcode/819. Most Common Word.py
+++ 25 Leetcode/819. Most Common Word.py
@@ -2,37 +2,13 @@ import math
 
 #def romanToInt(self, s):
 
-def same_row(word):
-    initial_row = 0
-    first = "qwertyuiop"
-    second = "asdfghjkl"
-    third = "zxcvbnm"
-    for i in range(len(word)):
-
-        cur_sim = word[i].lower()
-
-        if cur_sim in first:
-            cur_sim_row = 1
-        elif cur_sim in second:
-            cur_sim_row = 2
-        elif cur_sim in third:
-            cur_sim_row = 3
-
-        if initial_row == 0:
-            initial_row = cur_sim_row
-        else:
-            if initial_row != cur_sim_row:
-                return False
-    return True
-
-
-def findWords(words):
-    return list(filter(same_row, words))
+def mostCommonWord( paragraph, banned):
+    
 
 
 
 
 
-words = ["Hello","Alaska","Dad","Peace"]
-word = "Peace"
-print(findWords(words))
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+Output: "ball"
+print(mostCommonWord( paragraph, banned))
