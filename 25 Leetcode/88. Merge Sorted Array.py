4d2e2dbf55ed09be1deea7cc2c9b9992f938258f
odinicc <100484183+odinicc@users.noreply.github.com>
2023-01-16 19:31:31 +0300

    main

---
+++ 25 Leetcode/88. Merge Sorted Array.py
@@ -0,0 +1,13 @@
+import math
+
+#def twoSum(self, nums, target):
+def merge(self, nums1, m, nums2, n):
+
+
+
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+
+print(merge(nums1,len(nums1),nums2,len(nums2)))
+
+
